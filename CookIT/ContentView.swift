0219677c7921ee6c603a5de32acb1980cb5e7155
Marcel <baczynski.marcel@gmail.com>
2022-09-21 08:51:37 +0200

    Initial Commit

---
+++ CookIT/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CookIT
+//
+//  Created by Macbook on 21/09/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CookIT/CookITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CookITApp.swift
+//  CookIT
+//
+//  Created by Macbook on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct CookITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
