76f12c03d93c51b78b889c69cd2fe1dcb57d29ec
Marcel <baczynski.marcel@gmail.com>
2022-09-30 22:02:13 +0200

    first version of content view

--- CookIT/ContentView.swift
+++ CookIT/ContentView.swift
@@ -8,9 +8,82 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    init() {
+        UITabBar.appearance().backgroundColor = UIColor(Color("green"))
+        UITabBar.appearance().unselectedItemTintColor = UIColor(Color("white"))
+        UITabBar.appearance().isTranslucent = true
+        //UITabBar.appearance().selectedItem.foregroundColor(Color("white"))
+    }
+    
     var body: some View {
-        Text("bbbbbb")
-            .padding()
+        ZStack(alignment: .bottom) {
+            TabView {
+                DiscoverView()
+                    .tabItem {
+                        Label("Discover", systemImage: "house")
+                            .foregroundColor(Color("white"))
+                    }
+                FavoritesView()
+                    .tabItem {
+                        Label("Favorites", systemImage: "heart")
+                    }
+                FavoritesView()
+                    .tabItem {
+                        Label("Add Recipe", systemImage: "plus.square")
+                    }
+                FavoritesView()
+                    .tabItem {
+                        Label("Profile", systemImage: "person")
+                    }
+            }
+            .background(Color("white"))
+//            .frame(
+//                  minWidth: 0,
+//                  maxWidth: .infinity,
+//                  minHeight: 0,
+//                  maxHeight: .infinity,
+//                  alignment: .topLeading
+//            )
+            
+        .ignoresSafeArea()
+        }
+    }
+}
+
+struct DiscoverView: View {
+    var body: some View {
+        NavigationView {
+            List(recipes) {recipe in
+                NavigationLink {
+                    //RecipeDetail()
+                } label: {
+                    RecipeRow(recipe: recipe)
+                }
+            }
+            .navigationTitle("Discover")
+        }
+    }
+}
+struct FavoritesView: View {
+    var body: some View {
+        Text("Favorites View")
+            .font(.title)
+            .foregroundColor(.blue)
+    }
+}
+struct AddRecipeView: View {
+    var body: some View {
+        Text("Add Recipe")
+            .font(.title)
+            .foregroundColor(.blue)
+    }
+}
+struct ProfileView: View {
+    var body: some View {
+        Text("Profile")
+            .font(.title)
+            .foregroundColor(.blue)
     }
 }
 
