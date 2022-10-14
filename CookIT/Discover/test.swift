ddabd33ba90498ebab4100651cf485d3f04ae387
Marcel <baczynski.marcel@gmail.com>
2022-10-14 20:20:05 +0200

    test

---
+++ CookIT/Discover/test.swift
@@ -0,0 +1,20 @@
+//
+//  test.swift
+//  CookIT
+//
+//  Created by Macbook on 14/10/2022.
+//
+
+import SwiftUI
+
+struct test: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct test_Previews: PreviewProvider {
+    static var previews: some View {
+        test()
+    }
+}
