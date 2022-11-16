580bc89985f41e25f9eddb31d5e27de33cf00bac
Damian <damheros@gmail.com>
2022-11-16 20:39:14 +0100

    AddingRecipe

---
+++ CookIT/AddRecipe/AddRecipe.swift
@@ -0,0 +1,20 @@
+//
+//  AddRecipe.swift
+//  CookIT
+//
+//  Created by Damian Brudkowski on 16/11/2022.
+//
+
+import SwiftUI
+
+struct AddRecipe: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddRecipe_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipe()
+    }
+}
